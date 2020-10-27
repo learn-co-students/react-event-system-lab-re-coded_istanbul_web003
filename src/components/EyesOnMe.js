57e57aca883fa451eb86a778893327a37646a05f
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-27 00:13:24 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+class EyesOnMe extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleFocus =() => {
+    console.log('Good!');
+  }
+
+  handleBlur = () =>{
+    console.log('Hey! Eyes on me!')
+  }
+
+  render(){
+    return(
+      <button onFocus={this.handleFocus}  onBlur={this.handleBlur}>Yo</button>
+    )
+  }
+}
+
+export default EyesOnMe 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React, {Component} from 'react'
+
+class Keypad extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  handleKeyUp =() =>{
+    console.log('Entering password...');
+  }
+
+  render(){
+    return(
+      <input onKeyUp={this.handleKeyUp} type="password"></input>
+    )
+  }
+}
+
+export default Keypad 
