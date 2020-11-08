0811cd20eb14280cb2cc99d811c76cf72c0a7f6f
zeynepsen <zszynpsen@gmail.com>
2020-11-08 23:04:53 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from 'react'
+
+class EyesOnMe extends Component {
+
+    focus = () => {
+        console.log("Good!")
+    }
+
+    blur = () => {
+        console.log("Hey! Eyes on me!")
+    }
+
+    render() {
+        return (
+            <button onFocus={this.focus} onBlur={this.blur}/>
+        )
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+
+import React, { Component } from "react"
+
+class Keypad extends Component {
+    keyUp = () => {
+        console.log("Entering password...")
+    }
+
+render() {
+    return (
+        <input type= "password" onKeyUp={this.keyUp}/>   
+    )
+    }
+}
+
+export default Keypad;
